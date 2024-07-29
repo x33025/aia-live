210f0e100fc6c72bc2c2b935398f19a21eb308b8
x33025 <bulbs.carrots_0a@icloud.com>
2024-07-30 00:10:40 +0400

    updating the sockets so it works with vercel

--- src/lib/client/socket.ts
+++ src/lib/client/socket.ts
@@ -1,10 +1,11 @@
 import { io, type Socket } from 'socket.io-client';
+import { env } from '$env/dynamic/public'; // Importing public env variables from SvelteKit
 
 let socket: Socket | undefined;
 
 export function initializeSocket(userId: string): Socket {
   if (!socket) {
-    socket = io('http://localhost:3000', {
+    socket = io(env.PUBLIC_VERCEL_URL || 'http://localhost:3000', {
       query: {
         user_id: userId // Ensure userId is available
       }
