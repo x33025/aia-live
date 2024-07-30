7cf1d4eaf0c857810e65f8aef9a4d2c8fc08aacf
x33025 <bulbs.carrots_0a@icloud.com>
2024-07-31 01:47:40 +0400

    updating presence code

--- src/lib/stores/presence.ts
+++ src/lib/stores/presence.ts
@@ -3,29 +3,24 @@ import { writable } from 'svelte/store';
 import { supabase } from '$lib/config/supabase';
 import type { RealtimePresenceState } from '@supabase/supabase-js';
 
-interface Presence {
-  id: string;
-  online_at: string;
-}
-
-export const presences = writable<Presence[]>([]);
+export const presences = writable<RealtimePresenceState[]>([]);
 
 export const setupPresenceChannel = (room: string) => {
   const channel = supabase.channel(room);
 
-  channel.on('presence', { event: 'join' }, ({ newPresences }: { newPresences: Presence[] }) => {
+  channel.on('presence', { event: 'join' }, ({ newPresences }: { newPresences: RealtimePresenceState[] }) => {
     presences.update(current => [...current, ...newPresences]);
     console.log('Newly joined presences: ', newPresences);
   });
 
-  channel.on('presence', { event: 'leave' }, ({ leftPresences }: { leftPresences: Presence[] }) => {
+  channel.on('presence', { event: 'leave' }, ({ leftPresences }: { leftPresences: RealtimePresenceState[] }) => {
     presences.update(current => current.filter(p => !leftPresences.find(lp => lp.id === p.id)));
     console.log('Newly left presences: ', leftPresences);
   });
 
   channel.on('presence', { event: 'sync' }, () => {
-    const state: RealtimePresenceState<Presence> = channel.presenceState();
-    const presenceArray: Presence[] = Object.values(state).flat();
+    const state: RealtimePresenceState<any> = channel.presenceState();
+    const presenceArray = Object.values(state).flat();
     presences.set(presenceArray);
     console.log('Synced presence state: ', presenceArray);
   });
