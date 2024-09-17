8172f75d526d0690aa16e940b06e6d990ee426b6
x33025 <bulbs.carrots_0a@icloud.com>
2024-09-17 23:35:21 +0400

    - Added Images route.

---
+++ src/routes/protected/images/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">  
+import Stack from '$lib/components/layout/+stack.svelte';
+import { Direction } from '$lib/types';
+import RouteNavigation from '$lib/views/dashboard/+route-navigation.svelte';
+    import ImageChooser from '$lib/views/images/+image-chooser.svelte';
+
+
+</script>
+
+
+
+<Stack direction={Direction.Vertical} spacing={1}>
+ <ImageChooser />
+  
+</Stack>
