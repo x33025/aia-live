f9073b6adfd7ec0b73f0eb1dc37d8a563eabbcf2
x33025 <bulbs.carrots_0a@icloud.com>
2024-09-29 02:05:28 +0400

    - Added favicons.

--- src/lib/components/notes/+notes-button.svelte
+++ src/lib/components/notes/+notes-button.svelte
@@ -20,35 +20,32 @@
 
   // Function to sanitize HTML and extract text content
   function sanitizeHTML(html: string): string {
-      if (typeof document !== 'undefined') {
-          const div = document.createElement('div');
-          div.innerHTML = html;
-          return div.textContent || '';
-      }
-      return '';
+    if (typeof document !== 'undefined') {
+      const div = document.createElement('div');
+      div.innerHTML = html;
+      return div.textContent || '';
+    }
+    return '';
   }
 </script>
 
 <button on:click={openNotesSidebar}>
-<Stack direction={Direction.Horizontal} spacing={0.5} wrap={true}>
-
- 
-  {#if notes && notes.length > 0}
-
-    {#each notes as note (note.id)}
-      {#if note.expand?.activity?.created_by}
-        <Avatar size={1.5} userId={note.expand?.activity?.created_by} />
-      {/if}
-      {sanitizeHTML(note.content)}
-      {#if notes.length > 1}
-        <div style="border-left: 1px solid #e0e0e0; height: 100%;"></div>
-      {/if}
-    {/each}
-  {:else}
-  <Stack direction={Direction.Horizontal} style="background-color: var(--gray-1); padding: var(--default-padding); border-radius: 10em; " spacing={0.3} wrap={true}>
-  <PlusIcon size={0.7} color="var(--blue) " strokeWidth={0.3} />
-  <Text type={TextType.Caption} style="color: var(--blue); font-weight: bold;">Add a note</Text>
-</Stack>
-  {/if}
-</Stack>
+  <Stack direction={Direction.Horizontal} spacing={0.5} wrap={true} >
+    {#if notes && notes.length > 0}
+      {#each notes as note (note.id)}
+        {#if note.expand?.activity?.created_by}
+          <Avatar size={1.5} userId={note.expand?.activity?.created_by} />
+        {/if}
+        {sanitizeHTML(note.content)}
+        {#if notes.length > 1}
+          <div style="border-left: 1px solid var(--gray-2); height: 100%;"></div>
+        {/if}
+      {/each}
+    {:else}
+      <Stack direction={Direction.Horizontal} style="background-color: var(--gray-1); padding: var(--default-padding); border-radius: 10em;" spacing={0.3} wrap={true}>
+        <PlusIcon size={0.7} color="var(--blue)" strokeWidth={0.3} />
+        <Text type={TextType.Caption} style="color: var(--blue); font-weight: bold;">Add a note</Text>
+      </Stack>
+    {/if}
+  </Stack>
 </button>
