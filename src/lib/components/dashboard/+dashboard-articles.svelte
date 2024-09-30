00b68aba8372a8e249d91c53b52eb626401e07d8
x33025 <bulbs.carrots_0a@icloud.com>
2024-09-30 20:53:45 +0400

    - Updated dashboard articles.

--- src/lib/components/dashboard/+dashboard-articles.svelte
+++ src/lib/components/dashboard/+dashboard-articles.svelte
@@ -16,16 +16,16 @@
 </script>
 
 
-<div class="stack expand" style="--direction: column; --justify: start; border-left: 1px solid var(--gray-2);">
+<div class="stack expand" style="--direction: column; --justify: start; --gap: 0.5em; border-left: 1px solid var(--gray-2);">
 
   <div class="stack" style="--direction: row; --justify: space-between; padding: 0.5em;">
     <TimeFrameSelector />
   </div>
-  <div class="stack" style=" padding: 0.3em; border-top: 1px solid var(--gray-1)">
+  <div class="stack" style=" padding: 0.5em; border-top: 1px solid var(--gray-1)">
     {#if articles.length > 0} 
       {#each articles as article (article.id)}
 
-       
+       <div class="stack" style="--direction: column; --gap: 0.5em;">
           <div class="stack" style="--direction: row; --align: center; --justify: space-between; padding: 0.5em; background-color: var(--gray-1); border-radius: 0.5em;">
         
           {article.title} 
@@ -34,15 +34,16 @@
  
           <Avatar userId={article.author ?? 'defaultUserId'} size={1.75} />  
          
+          
 
         </div>
    
-      <div class="stack" style="--direction: row; --justify: space-between;">
-        <NotesButton notes={article.expand?.notes ?? []} parent={article} parent_collection="articles" />
-        <div class="spacer" />
-        <OpenArticleButton articleId={article.id} />
+        <div class="stack" style="--direction: row; --justify: space-between; --align: center; width: 100%;">
+          <NotesButton notes={article.expand?.notes ?? []} parent={article} parent_collection="articles" />
+      
+          <OpenArticleButton articleId={article.id} />
+        </div>
       </div>
-
       {/each}
     {:else}
       <Text type={TextType.Body} style="padding: 0.5em; background-color: var(--gray-1); border-radius: 0.5em; font-weight: 600;">
