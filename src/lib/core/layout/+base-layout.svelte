671dbad8fe12743f9c717b7a52e6be38d3ad19e1
x33025 <bulbs.carrots_0a@icloud.com>
2024-09-30 19:06:05 +0400

    - Updated Base layout.

--- src/lib/core/layout/+base-layout.svelte
+++ src/lib/core/layout/+base-layout.svelte
@@ -1,15 +1,11 @@
-
-
 <div class="stack expand" style="background-color: var(--gray-1); padding: 1em;">
-  
-  <div class="stack"  style="padding: 1em; width: 100%;" >
- 
-  <slot name="header" />
-  </div>
+  {#if $$slots.header}
+    <div class="stack" style="padding: 1em; width: 100%;">
+      <slot name="header" />
+    </div>
+  {/if}
 
-
-  <div  class="stack expand border-highlight" style="padding: 1em; background-color: white; border-radius: 1em;" >
- 
+  <div class="stack expand border-highlight" style="padding: 1em; background-color: white; border-radius: 1em;">
     <slot name="content" />
   </div>
 </div>
