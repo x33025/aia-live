c1eedbd23506e4a7ca4ec097c78b5389b9d86e7b
x33025 <bulbs.carrots_0a@icloud.com>
2024-10-05 13:34:27 +0400

    - Testing two way article subscription between article id and articles route

--- src/routes/protected/article/[id]/+layout@.svelte
+++ src/routes/protected/article/[id]/+layout@.svelte
@@ -2,13 +2,23 @@
 import { onMount } from 'svelte';
 import { article } from '$lib/stores/data/+articles'; 
 import { page } from '$app/stores';
+import { pb } from '$lib/config/pocketbase';
+import type { Article } from '$lib/types';
+
+
 
 onMount(() => {
+  article.set($page.data.article);
 
-  
-      article.set($page.data.article);
-  
+  // Subscribe to real-time changes in the articles collection
+  pb.collection('articles').subscribe('*', (e) => {
+    if (e.action === 'update' && e.record.id === $page.data.article.id) {
+      console.log('update', e.record);
+      article.set(e.record as unknown as Article); // Update only if it's the current article
+    } 
   });
+});
+
 </script>
 
 
