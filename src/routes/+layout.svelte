d000201e62908f6e3e6b00083ae4febe9d82f653
x33025 <bulbs.carrots_0a@icloud.com>
2024-10-20 03:25:46 +0400

    ⚛️ (routes/+layout.svelte): add onMount lifecycle method to show maintenance alert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,13 @@
   import { Sidebar } from '@x33025/components';
   import { Modal } from '@x33025/components';
   import { ContextMenu } from '@x33025/components';
+    import { onMount } from 'svelte';
 
+
+onMount(() => {
+
+  alert('UNDER MAINTENANCE');
+});
 </script>
 
 
